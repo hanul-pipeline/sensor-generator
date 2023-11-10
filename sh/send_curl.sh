75741a57b910f8ad456802a9fff486574e345566
hooniegit <neivekim76@gmail.com>
2023-11-11 01:12:24 +0900

    231111 🧑‍🏭 feat: send_curl 명령어 python 수정

--- sh/send_curl.sh
+++ sh/send_curl.sh
@@ -8,8 +8,8 @@ CURL_DIR="$SH_DIR/../src/curl"
 LOG_DIR="$SH_DIR/../logs/nohup"
 
 # 센서 스크립트 실행하기
-nohup python $CURL_DIR/location_7/send_100.py >> $LOG_DIR/100.log 2>&1 &
-nohup python $CURL_DIR/location_8/send_200.py >> $LOG_DIR/200.log 2>&1 &
-nohup python $CURL_DIR/location_10/send_300.py >> $LOG_DIR/300.log 2>&1 &
-nohup python $CURL_DIR/location_11/send_400.py >> $LOG_DIR/400.log 2>&1 &
-nohup python $CURL_DIR/location_7/send_500.py >> $LOG_DIR/500.log 2>&1 &
+nohup python3 $CURL_DIR/location_7/send_100.py >> $LOG_DIR/100.log 2>&1 &
+nohup python3 $CURL_DIR/location_8/send_200.py >> $LOG_DIR/200.log 2>&1 &
+nohup python3 $CURL_DIR/location_10/send_300.py >> $LOG_DIR/300.log 2>&1 &
+nohup python3 $CURL_DIR/location_11/send_400.py >> $LOG_DIR/400.log 2>&1 &
+nohup python3 $CURL_DIR/location_7/send_500.py >> $LOG_DIR/500.log 2>&1 &
