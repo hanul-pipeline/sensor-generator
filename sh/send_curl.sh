072e5cad0cdf2b38faafd4b849e23ebacf702400
hooniegit <neivekim76@gmail.com>
2023-12-01 13:03:17 +0900

    231201 👨‍🏭 fix: log 파일명 수정

--- sh/send_curl.sh
+++ sh/send_curl.sh
@@ -8,9 +8,9 @@ CURL_DIR="$SH_DIR/../src/curl"
 LOG_DIR="$SH_DIR/../logs/nohup"
 
 # 센서 스크립트 실행하기
-nohup python3 $CURL_DIR/location_7/send_100.py >> $LOG_DIR/100.log 2>&1 &
-nohup python3 $CURL_DIR/location_8/send_200.py >> $LOG_DIR/200.log 2>&1 &
-nohup python3 $CURL_DIR/location_10/send_300.py >> $LOG_DIR/300.log 2>&1 &
-nohup python3 $CURL_DIR/location_11/send_400.py >> $LOG_DIR/400.log 2>&1 &
-nohup python3 $CURL_DIR/location_7/send_500.py >> $LOG_DIR/500.log 2>&1 &
-nohup python3 $CURL_DIR/location_7/send_600.py >> $LOG_DIR/600.log 2>&1 &
+nohup python3 $CURL_DIR/location_7/send_100.py >> $LOG_DIR/send_100.log 2>&1 &
+nohup python3 $CURL_DIR/location_8/send_200.py >> $LOG_DIR/send_200.log 2>&1 &
+nohup python3 $CURL_DIR/location_10/send_300.py >> $LOG_DIR/send_300.log 2>&1 &
+nohup python3 $CURL_DIR/location_11/send_400.py >> $LOG_DIR/send_400.log 2>&1 &
+nohup python3 $CURL_DIR/location_7/send_500.py >> $LOG_DIR/send_500.log 2>&1 &
+nohup python3 $CURL_DIR/location_7/send_600.py >> $LOG_DIR/send_600.log 2>&1 &
